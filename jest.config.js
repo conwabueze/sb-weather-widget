cbfb71c6ab2afc20e895a87921e4d7c491674e9e
conwabueze <chinonso1nwabueze@gmail.com>
2022-11-07 16:36:53 -0500

    first commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFilesAfterEnv: ["./test/jest-setup.js"],
+  resetMocks: true,
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'jsdom',
+};
